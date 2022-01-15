9af460191757488b3b8fb69cc1534892f87db128
BRIGHTSON.MACHA <brightson.macha@equitybank.co.tz>
2022-01-15 11:45:01 +0300

    Project structure, Fragments, Navigation Graph

---
+++ app/src/main/java/com/example/a00highux/city/CityListFragment.kt
@@ -0,0 +1,19 @@
+package com.example.a00highux.city
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.a00highux.R
+
+
+class CityListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_city_list, container, false)
+
+        return view
+    }
+}
